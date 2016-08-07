37249e1665dc7f761de303d62799c584a3a6a317
Francesco Capuzzo <francesco.capuzzo@zoho.com>
2016-08-07 19:10:36 +0000

    linked to my firebase

--- src/core/firebase/index.ts
+++ src/core/firebase/index.ts
@@ -9,10 +9,14 @@ import {
 export const FIREBASE_APP_PROVIDERS: any[] = [
   FIREBASE_PROVIDERS,
   defaultFirebase({
-    apiKey: 'AIzaSyDaEW83qAOozjJbbJP1YYbEHxxfFksdSHQ',
+    apiKey: "AIzaSyA-a-CRC_HN8RpVlfiMaMbZk3gIjSC92XQ",
+    authDomain: "snappyvms.firebaseapp.com",
+    databaseURL: "https://snappyvms.firebaseio.com",
+    storageBucket: "snappyvms.appspot.com"
+    /*apiKey: 'AIzaSyDaEW83qAOozjJbbJP1YYbEHxxfFksdSHQ',
     authDomain: 'ng2-todo-app.firebaseapp.com',
     databaseURL: 'https://ng2-todo-app.firebaseio.com',
-    storageBucket: 'ng2-todo-app.appspot.com'
+    storageBucket: 'ng2-todo-app.appspot.com'*/
   }),
   firebaseAuthConfig({
     method: AuthMethods.Popup,
